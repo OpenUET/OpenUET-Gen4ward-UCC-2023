d0150b1e045ca21d9813c96f85432eab03e38c00
Lê Viết Đạt <69759053+datleviet@users.noreply.github.com>
2023-07-29 10:26:17 +0000

    Add basic Post API

--- server/api/routes/index.js
+++ server/api/routes/index.js
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 import authRouter from './auth.route.js';
+import postRouter from './post.route.js';
 
 const router = Router();
 
 router.use('/auth', authRouter);
+router.use('/posts', postRouter);
 
 export default router;
